30fbea92ea1122b855712c1c8753be9e2eb9098c
Sebastian <thenatoorat89@gmail.com>
2023-05-08 11:34:36 +0200

    finished project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-const navBtn = document.querySelector('.nav__btn')
-const navMenu = document.querySelector('.nav__mobile-list')
+const navMenu = document.querySelector('.nav__list');
+const burgerBtn = document.querySelector('.nav__btn');
+const mobileNavList = document.querySelectorAll('.nav__list-item');
+const footerYear = document.querySelector('.footer__year');
 
-navBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('nav__mobile-list--active')
-})
+burgerBtn.addEventListener('click', () => {
+	navMenu.classList.toggle('nav__list--active');
+	burgerBtn.classList.toggle('active');
+	document.body.classList.toggle('sticky-body');
+
+	mobileNavList.forEach((el) => {
+		el.addEventListener('click', () => {
+			navMenu.classList.remove('nav__list--active');
+			burgerBtn.classList.remove('active');
+			document.body.classList.remove('sticky-body');
+		});
+	});
+});
+
+const year = new Date().getFullYear();
+footerYear.innerHTML = year;
